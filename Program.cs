1390d24c45a3495403d0ffda9e8452a64221b14b
Matthew Berumen <mberumen2005@gmail.com>
2025-03-31 21:02:10 -0400

    added answer to the question

--- Program.cs
+++ Program.cs
@@ -1,2 +1,4 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿
+Console.WriteLine("GitHub is a website where developers store and manage their code. " +
+    "It helps track changes and allows multiple people to work on the same project. " +
+    "Developers use GitHub to save versions of their code and collaborate with others.");
